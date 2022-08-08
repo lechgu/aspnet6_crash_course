0ca2af633b2c16b1641d6e359f22ca3e6d563c91
Lech Gudalewicz <lechgu@gmail.com>
2022-08-08 11:40:32 -0700

    Hello, World

---
+++ App.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(opts =>
+{
+    opts.ListenAnyIP(9090);
+});
+var app = builder.Build();
+app.Map("/", () => "Hello, World");
+app.Run();
