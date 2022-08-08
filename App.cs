18d7adefd80fb558b4e67b36469c84396648f247
Lech Gudalewicz <lechgu@gmail.com>
2022-08-08 12:33:19 -0700

    Configuration

--- App.cs
+++ App.cs
@@ -1,11 +1,20 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using dotenv.net;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+DotEnv.Load();
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
+config.AddEnvironmentVariables();
+if (!int.TryParse(config["PORT"], out int port))
+{
+    port = 9090;
+}
 builder.WebHost.ConfigureKestrel(opts =>
 {
-    opts.ListenAnyIP(9090);
+    opts.ListenAnyIP(port);
 });
 builder.Services.AddControllers();
 var app = builder.Build();
